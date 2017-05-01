71fe0f387ccd3b085830b03bb6e86d58db8cf26c
diegob <diego91bernal@gmail.com>
2017-04-30 17:10:26 -0700

    Added package.json. Added gulp task for production build

--- _dist/js/function.js
+++ _dist/js/function.js
@@ -8,33 +8,35 @@
 	Gellery Portolio
 	Date Picker
 	Form Validation
-	
+
  */
 
 $(document).ready(function () {
 	'use strict';
-	
+
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$('body').on('click', '.page-scroll a', function(event) {
 			var $anchor = $(this);
+			var scrollTopValue = $($anchor.attr('href')).offset().top;
+			var scrollTo = scrollTopValue < 800 ? 0 : scrollTopValue;
 			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
+				scrollTop: scrollTo - 50
 			}, 1500, 'easeInOutExpo');
 			event.preventDefault();
-		
+
 		});
 	});
-	
+
 	//Main slider fuction
 	$('.carousel').carousel({
   		interval: 3000
-		
+
 	})
-	
+
 	// Parallaxe  function
-	$('#parallax-2').parallax("50%",0.5);
-	
+	//$('#parallax-2').parallax("50%",0.5);
+
 	// Carousel mini slider function
 	$('.responsive').slick({
 	  dots: false,
@@ -76,9 +78,9 @@ $(document).ready(function () {
 		  }
 		}
 	  ]
-	});	
-			
-	// mobile menu function		
+	});
+
+	// mobile menu function
 	$("#mobile-menu").mobileMenu({
 			MenuWidth: 250,
 			SlideSpeed : 300,
@@ -88,102 +90,47 @@ $(document).ready(function () {
 			Overlay : true,
 			CollapseMenu : true,
 			ClassName : "mobile-menu"
-		});	
-		
-	// Gellery Portfolio function		
-	$('.fancybox').fancybox();
-	// Gellery function end	
-	
-	//date picker function
-		$('#datePicker').datepicker({
-					format: 'dd/mm/yyyy',
-					autoclose: true,
-					clearBtn: true,
-					todayHighlight: true
-		})
-		//.on('changeDate', function(e) {
-		// Revalidate the date field
-		//$('#AppointmentForm').bootstrapValidator('revalidateField', 'date');
-		.on('change', function() {
-        $('#datePicker').valid();  // triggers the validation test on change
+		});
 
-	});
-	
-	////AppointmentForm//////////
-	$("#AppointmentFrm").validate({
-		errorElement: "p",
-		submitHandler: function(){
-			var dataString = $("form#AppointmentFrm").serialize();
-			$.ajax({
-			type: "POST",
-			url: "appointmentsend.php",
-			cache: false,
-			data: dataString,
-			success:function(data)
-			{	
-				if(data==1)
-				{	
-					$("#SuccessMessage").slideDown(500);
-					$("#SuccessMessage").html("<div class='alert alert-success'><strong>Success!</strong> Your message has been sent to us.</div>");
-					//document.getElementById("SuccessMessage").style.color='#FF0000';
-					//document.getElementById('AppointmentFrm').reset();
-					$("#AppointmentFrm")[0].reset();
-					return false;
-					
-				}
-				else
-				{
-					$("#ErrorMessage").slideDown(500);
-					$("#ErrorMessage").html("<div class='alert alert-danger'><strong>Error!</strong> There was an error sending your message.</div>");
-					//document.getElementById("ErrorMessage").style.color='#FF0000';
-					//document.getElementById('AppointmentFrm').reset();
-					$("#AppointmentFrm")[0].reset();
-					return false;
-					
-				}
-			},
-			
-		}); 
-		return false;
-		}
-	});
-	///AppointmentForm///////////
+	// Gellery Portfolio function
+	$('.fancybox').fancybox();
+	// Gellery function end
 
 	//////ContactForm////////
-	$("#ContactForm").validate({
-		errorElement: "p",
-		submitHandler: function(){
-			var dataString = $("form#ContactForm").serialize();
-			$.ajax({
-			type: "POST",
-			url: "contactsend.php",
-			cache: false,
-			data: dataString,
-			success:function(data)
-			{	
-				if(data==1)
-				{	 
-					$("#ContactSuccessMessage").slideDown(500);
-					$("#ContactSuccessMessage").html("<div class='alert alert-success'><strong>Success!</strong> Your message has been sent to us.</div>");
-					//document.getElementById("ContactSuccessMessage").style.color='#FF0000';
-					$("#ContactForm")[0].reset();
-					return false;
-				}
-				else
-				{
-					$("#ContactErrorMessage").slideDown(500);
-					$("#ContactErrorMessage").html("<div class='alert alert-danger'><strong>Error!</strong> There was an error sending your message.</div>");
-					//document.getElementById("ContactErrorMessage").style.color='#FF0000';
-					$("#ContactForm")[0].reset();
-					return false;
-				}
-			},
-			
-		}); 
-		return false;
-		}
-	});
-	
+	// $("#ContactForm").validate({
+	// 	errorElement: "p",
+	// 	submitHandler: function(){
+	// 		var dataString = $("form#ContactForm").serialize();
+	// 		$.ajax({
+	// 		type: "POST",
+	// 		url: "contactsend.php",
+	// 		cache: false,
+	// 		data: dataString,
+	// 		success:function(data)
+	// 		{
+	// 			if(data==1)
+	// 			{
+	// 				$("#ContactSuccessMessage").slideDown(500);
+	// 				$("#ContactSuccessMessage").html("<div class='alert alert-success'><strong>Success!</strong> Your message has been sent to us.</div>");
+	// 				//document.getElementById("ContactSuccessMessage").style.color='#FF0000';
+	// 				$("#ContactForm")[0].reset();
+	// 				return false;
+	// 			}
+	// 			else
+	// 			{
+	// 				$("#ContactErrorMessage").slideDown(500);
+	// 				$("#ContactErrorMessage").html("<div class='alert alert-danger'><strong>Error!</strong> There was an error sending your message.</div>");
+	// 				//document.getElementById("ContactErrorMessage").style.color='#FF0000';
+	// 				$("#ContactForm")[0].reset();
+	// 				return false;
+	// 			}
+	// 		},
+	//
+	// 	});
+	// 	return false;
+	// 	}
+	// });
+
 	// portfolio 4 page function
 	$(window).load(function(){
 	var $container = $('.blogmasonary');
@@ -195,11 +142,11 @@ $(document).ready(function () {
             queue: true
         }
     });
-	
-	
+
+
 	var $container = $('.portfoliodiv');
     $container.isotope({
-        filter: '*',
+        filter: '.coloring',
 		layoutMode: 'masonry',
         animationOptions: {
             duration: 750,
@@ -207,11 +154,10 @@ $(document).ready(function () {
             queue: true
         }
     });
-	 
+
     $('.filter ul li a').click(function(){
         $('.filter .active').removeClass('active');
         $(this).addClass('active');
- 
         var selector = $(this).attr('data-filter');
 			if(selector=="*") $(selector).children("a.fancybox").attr("data-fancybox-group","gallery" );
 		else	$(selector).children("a.fancybox").attr("data-fancybox-group",selector.substring(1) );
@@ -223,12 +169,12 @@ $(document).ready(function () {
             }
          });
          return false;
-    }); 
-	
+    });
+
 	});
 	// animation
-	 
-	 
+
+
 	//scroll top function
 	$(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
@@ -244,11 +190,10 @@ $(document).ready(function () {
         }, 800);
         return false;
     });
-	
+
 });
  $(window).load(function() {
-	 
+
 	 new WOW().init();
-	 });   
- //hide all the div containers inside div with id=accordion  
-       
+	 });
+ //hide all the div containers inside div with id=accordion
